ba80ff68b23f6904121994d5a0a465f501971825
Scott Yak <scott.yak.xy@gmail.com>
2021-12-10 02:30:43 -0500

    Make the bottom panel look a bit nicer

--- armi/notebooks/grid_gui_lib.js
+++ armi/notebooks/grid_gui_lib.js
@@ -1,18 +1,14 @@
-function define_nav_bar(nav_bar) {
-    nav_bar.attr("class", "navbar navbar-expand-sm bg-dark navbar-dark");
+function defineNavBar(navBar) {
+    navBar.attr("class", "navbar navbar-expand-sm bg-dark navbar-dark");
 
-    nav_bar.append("a")
+    navBar.append("a")
         .attr("class", "navbar-brand")
         .attr("href", "#")
-        .text("Terrapower ARMI");
+        .text("Grid Editor");
 }
 
-function update_draw_chart(svg, data) {
+function updateDrawChart(svg, data) {
     const points = data["points"];
-    // const data = [
-    //     {x_pos:0,y_pos:0,x_dat:0,y_dat:0}, {x_pos:0,y_pos:1,x_dat:0,y_dat:0},
-    //     {x_pos:1,y_pos:0,x_dat:0,y_dat:0}, {x_pos:1,y_pos:1,x_dat:0,y_dat:0},
-    // ];
     var g = svg.selectAll("g").data(points).enter().append("g");
 
     g.attr("width", 40).attr("height", 40);
@@ -35,155 +31,169 @@ function update_draw_chart(svg, data) {
         .attr("class", "grid-gui-gridcell");
 }
 
-function define_display_area(display_area, data) {
-    const height_gui_box = 600;
-    const width_gui_box = 600;
+function defineDisplayArea(displayArea, data) {
+    const widthGuiBox = 700;
+    const heightGuiBox = 700;
 
-    display_area.append("h3").text("Display Area");
-    var svg = display_area.append("svg")
+    var svg = displayArea.append("svg")
         .attr("id", "grid-area")
-        .attr("width", width_gui_box)
-        .attr("height", height_gui_box);
+        .attr("width", widthGuiBox)
+        .attr("height", heightGuiBox);
         
     svg.append("rect")
         .attr("x", 0)
         .attr("y", 0)
-        .attr("width", width_gui_box)
-        .attr("height", height_gui_box)
-        .attr("stroke", "black")
-        .attr("stroke-width", "2px")
+        .attr("width", widthGuiBox)
+        .attr("height", heightGuiBox)
         .style("fill", "none");
 
-    update_draw_chart(svg, data);
+    updateDrawChart(svg, data);
 }
 
-function define_side_panel(side_panel) {
-    side_panel
+function defineSidePanel(sidePanel) {
+    sidePanel
         .append("h3").text("Assemblies")
-        .append("p").text("Equilbrium Fuel Path");
+        .append("p").text("Equilibrium Fuel Path");
 
-    side_panel.append("button")
+    sidePanel.append("button")
         .attr("type", "button")
         .attr("class", "btn-btn")
         .attr("id", "fuel_path")
         .text("Fuel Path");
 
-    side_panel.append("button")
+    sidePanel.append("button")
         .attr("type", "button")
         .attr("class", "btn-btn")
         .attr("id", "rm_fuel_path")
         .text("Remove From Fuel Path");
 }
 
-function define_middle_container(middle_container, data) {
-    var row = middle_container
+function defineMiddleContainer(middleContainer, data) {
+    var row = middleContainer
         .attr("class", "container")
         .append("div")
             .attr("class", "row");
-    var display_area = row.append("div")
+    var displayArea = row.append("div")
         .attr("class", "w-75 p-3");
-    define_display_area(display_area, data);
+    defineDisplayArea(displayArea, data);
 
-    var side_panel = row.append("div")
+    var sidePanel = row.append("div")
         .attr("class", "w-25 p-3")
         .attr("id", "side-panel");
-    define_side_panel(side_panel);
+    defineSidePanel(sidePanel);
 }
 
-function define_bottom_panel(bottom_panel) {
-    var bottom_box_area = bottom_panel
-        .attr("class", "container")
+function defineBottomPanel(bottomPanel) {
+    var bottomBoxArea = bottomPanel
         .attr("id", "bottom-box-area");
-    
-    bottom_box_area.append("input")
+
+    bottomBoxArea.append("div")
+        .style("width", "100%")
+        .text("Num. Rings");
+
+    bottomBoxArea.append("div").append("button")
+        .attr("type", "button")
+        .attr("class", "btn-btn")
+        .text("Expand to Full Core");
+
+    bottomBoxArea.append("div").append("button")
+        .attr("type", "button")
+        .attr("class", "btn-btn")
+        .text("Save Grid Blueprints")
+        .on("click", () => {
+            const a = document.createElement('a');
+            const file = new Blob(["Dummy Blueprint YAML file contents"], {type: 'text/plain'});
+            a.href = URL.createObjectURL(file);
+            a.download = "my-blueprint.yaml";
+            a.click();
+            URL.revokeObjectURL(a.href);
+        });
+
+    bottomBoxArea.append("div").append("button")
+        .attr("type", "button")
+        .attr("class", "btn-btn")
+        .attr("id", "apply_button")
+        .text("Help");
+
+    bottomBoxArea.append("div").append("button")
+        .attr("type", "button")
+        .attr("class", "btn-btn")
+        .attr("id", "apply_button")
+        .text("Save Image...");
+
+    bottomBoxArea.append("div").append("input")
         .attr("type", "number")
-        .attr("id", "buttonSize")
-        .attr("value", 13);
+        .attr("id", "numRings")
+        .style("height", "90%")
+        .attr("min", 1)
+        .attr("max", 20)
+        .attr("value", 5)
+        .attr("title", "Select how many rings of the grid to display");
 
-    const gridGuiModeOptions = ["Specifier", "Shuffle Path", "(i,j)", "(Ring, Position)"];
-    var gridGuiModeButton = bottom_box_area.append('select');
+    const modeOptions = ["Specifier", "Shuffle Path", "(i,j)", "(Ring, Position)"];
+    var modeSelector = bottomBoxArea.append("div").append("select")
+        .style("width", "100%")
+        .style("height", "90%")
+        .style("text-align", "center");
 
-    gridGuiModeButton // Add a button
-        .selectAll('myOptions') // Next 4 lines add 6 options = 6 colors
-        .data(gridGuiModeOptions)
+    modeSelector // Add a button
+        .selectAll("myOptions") // Next 4 lines add 6 options = 6 colors
+        .data(modeOptions)
         .enter()
-        .append('option')
+        .append("option")
         .text((d) => (d)) // text showed in the menu
-        .attr("value", (d) => (d)); // corresponding value returned by the button
+        .attr("value", (d) => (d)) // corresponding value returned by the button
+        .on("change", () => {
+            // recover the option that has been chosen
+            const selectedOption = modeSelector.property("value");
+            // refer to https://www.d3-graph-gallery.com/graph/interactivity_button.html
 
+            // run the updateChart function with this selected option
+            // Put the function here
+            let comm = new CommAPI("update_graph", (ret)=>{alert("The returned value is " + ret.results["n"])});
+            comm.call({n: selectedOption});
+            // updateChart(selectedOption)
+        });
 
-    // A function that update the color of the circle. Run from dropdown Button
-    // function updateChart(mycolor) {
-    //  zeCircle
-    //    .transition()
-    //    .duration(1000)
-    //    .style("fill", mycolor)
-    //}
+    bottomBoxArea.append("div").append("label")
+        .attr("for", "open_blueprint_btn")
+        .attr("class", "btn-btn")
+        .style("width", "100%")
+        .style("text-align", "center")
+        .text("Open Grid Blueprints")
+        .append("input")
+        .attr("id", "open_blueprint_btn")
+        .attr("type", "file")
+        .attr("class", "invisible");
 
-    // When the button is changed, run the updateChart function
-    gridGuiModeButton.on("change", function(d) {
+    bottomBoxArea.append("div");
+    bottomBoxArea.append("div");
 
-        // recover the option that has been chosen
-        var selectedOption = d3.select(this).property("value");
-        // refer to https://www.d3-graph-gallery.com/graph/interactivity_button.html
-
-        // run the updateChart function with this selected option
-        // Put the function here
-        let comm = new CommAPI("update_graph", (ret)=>{alert("The returned value is " + ret.results)})
-        comm.call({n: 3})
-        // updateChart(selectedOption)
-    });
-
-    // https://stackoverflow.com/questions/15244182/d3-create-buttons-from-an-array-of-string-containing-names
-    bottom_box_area.append('button')
+    bottomBoxArea.append("button")
         .attr("type", "button")
         .attr("class", "btn-btn")
         .attr("id", "apply_button")
         .text("Apply");
 
-    bottom_box_area.append("button")
-        .attr("type", "button")
-        .attr("class", "btn-btn")
-        .attr("id", "apply_button")
-        .text("Expand to Full Core");
+    bottomBoxArea.append("div");
 
-    bottom_box_area.append('button')
-        .attr("type", "button")
-        .attr("class", "btn-btn")
-        .attr("id", "apply_button")
-        .text("Save Grid Blueprints");
-
-    bottom_box_area.append('button')
-        .attr("type", "button")
-        .attr("class", "btn-btn")
-        .attr("id", "apply_button")
-        .text("Open Grid Blueprints");
-
-    bottom_box_area.append('button')
+    bottomBoxArea.append("button")
         .attr("type", "button")
         .attr("class", "btn-btn")
         .attr("id", "apply_button")
         .text("New Grid Blueprints");
-
-    bottom_box_area.append('button')
-        .attr("type", "button")
-        .attr("class", "btn-btn")
-        .attr("id", "apply_button")
-        .text("Help");
 }
 
 function main(id, data) {
     const root = d3.select(id);
-    root.append("meta").attr("charset", "utf-8");
-    root.append("meta").attr("name", "viewport").attr("content", "width=device-width, initial-scale=1");
+    root.attr("class", "gray-background");
 
-    const nav_bar = root.append("nav");
-    define_nav_bar(nav_bar);
-    root.append("div").attr("class", "jumbotron text-center").append("h1").text("Grid Editor");
+    const navBar = root.append("nav");
+    defineNavBar(navBar);
 
-    const middle_container = root.append("div");
-    define_middle_container(middle_container, data);
+    const middleContainer = root.append("div");
+    defineMiddleContainer(middleContainer, data);
 
-    const bottom_panel = root.append("div");
-    define_bottom_panel(bottom_panel);
+    const bottomPanel = root.append("div");
+    defineBottomPanel(bottomPanel);
 }
